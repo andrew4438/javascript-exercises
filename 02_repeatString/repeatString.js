e0c1b399249a17279ede1a3e74694a7b63545655
Andrew4438 <andrew.hartley@live.co.uk>
2022-10-23 15:27:46 +0100

    Complete repeatStrings exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
-
+const repeatString = (inputString, num) => {
+    if (num < 0) return 'ERROR';
+    let outputString = '';
+    for (let i=0; i<num; i++) {
+        outputString += inputString;
+    }
+    return outputString;
 };
+
+// ORIGINAL METHOD
+// const repeatString = function(string, num) {
+//     outputString = '';
+//     if (num >= 0) {
+//         for (let i=0; i<num; i++) {
+//             outputString = outputString + string;
+//         }
+//     } else {
+//         outputString = 'ERROR';
+//     }
+//     return outputString;
+// };
 
 // Do not edit below this line
 module.exports = repeatString;
