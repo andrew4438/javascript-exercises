dee788d07d91e4d8dc8c7ffe1bd0745387465b38
Andrew4438 <andrew.hartley@live.co.uk>
2022-10-23 16:51:30 +0100

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = (inputString) => {
+    let outputString = '';
+    for (let i = inputString.length - 1; i > -1; i--) {
+        outputString += inputString[i];
+    }
+    return outputString;
 };
 
 // Do not edit below this line
