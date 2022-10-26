0435c6c1ad1f47c3562d61150739fb4ff5c0722e
Andrew4438 <andrew.hartley@live.co.uk>
2022-10-26 21:18:10 +0100

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const ftoc = function() {
+const ftoc = (f) => {
+
+  let c = (f - 32) * (5 / 9);
+  c = c * 10;
+  c = Math.round(c);
+  c = c / 10;
+  return c;
 
 };
 
-const ctof = function() {
+const ctof = (c) => {
 
+  let f = (c * (9 / 5)) + 32;
+  f = f * 10;
+  f = Math.round(f);
+  f = f / 10;
+  return f;
 };
 
 // Do not edit below this line
