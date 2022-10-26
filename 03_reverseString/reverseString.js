2411e81b27c809f4dc7588bb69842c144ae9733c
Andrew4438 <andrew.hartley@live.co.uk>
2022-10-26 16:25:10 +0100

    Complete removeFromArray exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,16 @@
 const reverseString = (inputString) => {
-    let outputString = '';
-    for (let i = inputString.length - 1; i > -1; i--) {
-        outputString += inputString[i];
-    }
-    return outputString;
-};
+    return inputString.split('').reverse().join('');
+}
+
+
+// MY ORIGINAL SOLUTION
+// const reverseString = (inputString) => {
+//     let outputString = '';
+//     for (let i = inputString.length - 1; i > -1; i--) {
+//         outputString += inputString[i];
+//     }
+//     return outputString;
+// };
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = (inputArray, ...searchItems) => {
 
-};
+    // console.log(`inputArray = ${inputArray}`);
+    // console.log(`searchItems = ${searchItems}`);
 
-// Do not edit below this line
+    const outputArray = [];
+
+    for (let i=0; i<inputArray.length; i++) {
+        // console.log(`i=${i}, searchItem=${inputArray[i]}`);
+
+        if (!searchItems.includes(inputArray[i])) {
+            outputArray.push(inputArray[i]);
+            // console.log(outputArray);
+        }
+    }
+    // console.log('\n');
+    return outputArray;
+}
+
 module.exports = removeFromArray;
+
+// removeFromArray([1, 2, 3, 4], 3);
+// removeFromArray([1, 2, 3, 4], 3, 2)
+// removeFromArray([1, 2, 3, 4], 7, "tacos")
+// removeFromArray([1, 2, 3, 4], 7, 2)
+// removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)
+// removeFromArray(["hey", 2, 3, "ho"], "hey", 3)
+// removeFromArray([1, 2, 3], "1", 3)
