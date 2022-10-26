d3435bac24fab04d515a0a136a6e32e56f8164a8
Andrew4438 <andrew.hartley@live.co.uk>
2022-10-26 17:52:08 +0100

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = (integer1, integer2) => {
 
+    let lowerNumber = integer1;
+    let upperNumber = integer2;
+
+    if (integer1 == integer2) {
+        return 'ERROR';
+    } else if (integer1 < 0 || integer2 < 0) {
+        return 'ERROR';
+    } else if (typeof integer1 !== 'number' || typeof integer2 !== 'number') {
+        return 'ERROR';
+    } else if (integer1 > integer2) {
+        lowerNumber = integer2;
+        upperNumber = integer1;
+    };
+
+
+    let sum = 0;
+    for (let i = lowerNumber; i < upperNumber + 1; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
