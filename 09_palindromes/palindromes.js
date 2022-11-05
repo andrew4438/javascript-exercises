0d68f9ddb3e3f6f6702e0448ae655b6939fcc19a
Andrew4438 <andrew.hartley@live.co.uk>
2022-11-05 17:10:11 +0000

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = (forwardString) => {
+    
 
+    forwardString = forwardString.replaceAll(/[!,.\s]/g, '').toLowerCase();
+    let forwardArray = forwardString.split('');
+    let reverseArray  = forwardArray.reverse();
+    let reverseString = reverseArray.join('');
+
+    if (forwardString === reverseString) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+// TEXTBOOK SOLUTION //
+// const palindromes = function(string) {
+    
+//     processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+//     return (
+//         processedString.split("").reverse().join("") == processedString
+//     );
+// };
 
 // Do not edit below this line
 module.exports = palindromes;
