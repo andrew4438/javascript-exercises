50195ead78e3704e95f10e6b526ff360f05baee9
Andrew4438 <andrew.hartley@live.co.uk>
2022-11-05 17:42:18 +0000

    Complete getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+
+    const titles = books.map(book => book.title);
+    return titles;
 
 };
+
+// TEXTBOOK ANSWER //
+// const getTheTitles = function(array) {
+//     return array.map(book => book.title);
+// };
 
 // Do not edit below this line
 module.exports = getTheTitles;
