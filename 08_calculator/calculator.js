3560eb9e9bbe5ba8c7d6785392a5da112d278a44
Andrew4438 <andrew.hartley@live.co.uk>
2022-11-05 14:56:10 +0000

    Complete factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = (a, b) => {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = (a, b) => {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total=0;
+  for (let i=0; i < array.length; i++) {
+    total += array[i];
+  };
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = (array) => {
+  let total = 1;
+  for (let i=0; i < array.length; i++) {
+    total = total * array[i];
+  };
+  return total;
 };
 
-const power = function() {
-	
+const power = (a, b) => {
+  let total = a;
+  for (let i=1; i<b; i++) {
+    total = total * a;
+  };
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  for (let i=1; i<a+1; i++) {
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
