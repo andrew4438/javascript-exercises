dbe69c7521ddde12c4bef16ee6a9736c4f995956
Andrew4438 <andrew.hartley@live.co.uk>
2022-11-05 17:28:21 +0000

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+    if (n<0) return 'OOPS';
 
+    let fibonacciArray = [1,1];
+
+    for (let i=2; i<n+1; i++) {
+        fibonacciArray.push(fibonacciArray[i-1] + fibonacciArray[i-2]);
+    };
+    return fibonacciArray[n-1];
 };
+
+// TEXTBOOK SOLUTION //
+// const fibonacci = function(count) {
+//     if (count < 0) return "OOPS";
+//     if (count === 0 ) return 0;
+
+//     let a = 0;
+//     let b = 1;
+
+//     for (let i=1; i<count; i++) {
+//         const temp = b;
+//         b = a + b;
+//         a = temp;
+//     }
+//     return b;
+// };
 
 // Do not edit below this line
 module.exports = fibonacci;
